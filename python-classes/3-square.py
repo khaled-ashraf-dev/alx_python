520643b5064500ca0e7530c0235846f442be8a06
Your Name <you@example.com>
2023-08-03 18:28:10 +0300

    fixing stuff

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -33,5 +33,16 @@ class Square:
     def size(self):
         return self.__size
 
+    @size.setter
+    def size(self, value):
+        
+        if not isinstance(self.value, int):
+            raise TypeError('size must be an integer')
+        
+        elif self.value < 0:
+            raise ValueError('size must be >= 0')
+        
+        self.__size = value
+
     def area(self):
         return self.__size ** 2
