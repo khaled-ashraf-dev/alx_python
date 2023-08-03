b8448065672b137af81eb5b2dd35833973b5e0f7
Your Name <you@example.com>
2023-08-03 17:13:39 +0300

    0 task

---
+++ 0-square.py
@@ -0,0 +1,12 @@
+class Square:
+    def __init__(self, size):
+        """
+        Constructor for the Square class.
+
+        Parameters:
+        size (int): The size of the square's side.
+
+        Initializes a new Square object with the specified size.
+        The size is assigned to the private instance variable __size.
+        """
+        self.__size = size
