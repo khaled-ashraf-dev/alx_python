cf645cffd03c28ab1cbeb427775b4d8088310bd3
Your Name <you@example.com>
2023-08-03 17:44:42 +0300

    fixed doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,6 +3,15 @@ A class representing a square.
 """
 
 class Square:
+    """
+        Constructor for the Square class.
+
+        Parameters:
+        size (int): The size of the square's side.
+
+        Initializes a new Square object with the specified size.
+        The size is assigned to the private instance variable __size.
+        """
     def __init__(self, size):
         """
         Constructor for the Square class.
@@ -14,3 +23,4 @@ class Square:
         The size is assigned to the private instance variable __size.
         """
         self.__size = size
+
