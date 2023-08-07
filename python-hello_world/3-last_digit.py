88472cb97ba7e6010e1a615f9c93c1618535c565
Your Name <you@example.com>
2023-08-07 19:57:17 +0300

    fixed task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = int(str(number)[-1])
+if number < 0:
+    last_digit *= -1
+
 result = "Last digit of {} is {}".format(number, last_digit)
+
 if last_digit > 5:
     result += ' and is greater than 5'
 elif last_digit == 0:
