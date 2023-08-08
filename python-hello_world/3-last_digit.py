e24f2363956b481a2fb9b70c700aa5b729bf5732
Your Name <you@example.com>
2023-08-08 14:10:42 +0300

    fixed last digit task

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,11 +7,9 @@ if not number and number != 0:
 
 if number < 0:
     last_digit = number % -10
+    
 else:
     last_digit = number % 10
-
-if number < 0:
-    last_digit *= -1
 
 result = "Last digit of {} is {}".format(number, last_digit)
 
