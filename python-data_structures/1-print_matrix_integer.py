d1d148edff618397e67eea29f189bad9a2afa5e9
Your Name <you@example.com>
2023-08-09 11:57:49 +0300

    task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,12 @@
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print()
-        return None
     
     for row in matrix:
+
         for i in range(len(row)):
             if i != len(row) - 1:
                 print('{:d}'.format(row[i]), end=' ')
             else:
                 print('{:d}'.format(row[i]))
+
+        else:
+            print()
