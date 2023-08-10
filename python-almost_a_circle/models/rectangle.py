2003683ff0c7f89db740b80234873da987fccb5f
Your Name <you@example.com>
2023-08-10 17:33:12 +0300

    task 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,9 @@
+from base import Base
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        self.__id = Base.id
