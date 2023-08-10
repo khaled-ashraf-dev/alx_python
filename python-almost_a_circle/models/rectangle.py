1e2e4c46dc25feffb94f68e2f596199c606dba0d
Your Name <you@example.com>
2023-08-10 17:34:04 +0300

    task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,49 @@
 from base import Base
 
+"""
+    Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+"""
 class Rectangle(Base):
+    """
+    Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+    Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+    """
         self.__width = width
         self.__height = height
         self.__x = x
