4bf872157509336a69f67336188110d299fa85c8
Your Name <you@example.com>
2023-08-10 17:43:47 +0300

    task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -29,6 +29,19 @@ class Rectangle(Base):
         Attributes:
             __nb_objects (int): A private class-level attribute to track the number of objects created.
     """
+    def __init__(self):
+        """
+        Initializes an instance of the Base class.
+
+        Args:
+            id (int): An optional identifier to assign to the instance.
+        Initializes an instance of the Base class.
+
+        Args:
+            id (int): An optional identifier to assign to the instance.
+        """
+        super().__init__()  # Call the constructor of the parent class
+    
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Initializes an instance of the Base class.
@@ -44,4 +57,3 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-        self.__id = Base.id
