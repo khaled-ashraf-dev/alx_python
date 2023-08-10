887f65c6f304dd4af8df1334e2573b72f3fe23ee
Your Name <you@example.com>
2023-08-10 17:36:46 +0300

    task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,34 +16,30 @@ from base import Base
 """
 class Rectangle(Base):
     """
-    Represents a base entity.
-
-    Attributes:
-        __nb_objects (int): A private class-level attribute to track the number of objects created.
         Represents a base entity.
 
-    Attributes:
-        __nb_objects (int): A private class-level attribute to track the number of objects created.
-        Represents a base entity.
+        Attributes:
+            __nb_objects (int): A private class-level attribute to track the number of objects created.
+            Represents a base entity.
 
-    Attributes:
-        __nb_objects (int): A private class-level attribute to track the number of objects created.
+        Attributes:
+            __nb_objects (int): A private class-level attribute to track the number of objects created.
+            Represents a base entity.
+
+        Attributes:
+            __nb_objects (int): A private class-level attribute to track the number of objects created.
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-    Represents a base entity.
+        Initializes an instance of the Base class.
 
-    Attributes:
-        __nb_objects (int): A private class-level attribute to track the number of objects created.
-        Represents a base entity.
+        Args:
+            id (int): An optional identifier to assign to the instance.
+        Initializes an instance of the Base class.
 
-    Attributes:
-        __nb_objects (int): A private class-level attribute to track the number of objects created.
-        Represents a base entity.
-
-    Attributes:
-        __nb_objects (int): A private class-level attribute to track the number of objects created.
-    """
+        Args:
+            id (int): An optional identifier to assign to the instance.
+        """
         self.__width = width
         self.__height = height
         self.__x = x
