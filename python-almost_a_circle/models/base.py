dab369108a6954f82572bba2e94c6269cfe675e6
Your Name <you@example.com>
2023-08-10 17:16:34 +0300

    task 0

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,19 @@
 class Base:
+    """
+    Represents a base entity.
+
+    Attributes:
+        __nb_objects (int): A private class-level attribute to track the number of objects created.
+    """
+
     __nb_objects = 0
     
     def __init__(self, id=None):
-        if id != None:
+        """
+        Initializes an instance of the Base class.
+
+        Args:
+            id (int): An optional identifier to assign to the instance.
+        """
+        if id is not None:
             self.id = id
