241c2be3fb9c8f0835b249db136eb25e46e274ef
Your Name <you@example.com>
2023-08-10 19:17:54 +0300

    task 1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -44,6 +44,20 @@ class Square(Rectangle):
             __nb_objects (int): A private class-level attribute to track the number of objects created.
     """    
     def __init__(self, size, x=0, y=0, id=None):
+        """
+            Represents a base entity.
+
+            Attributes:
+                __nb_objects (int): A private class-level attribute to track the number of objects created.
+                Represents a base entity.
+
+            Attributes:
+                __nb_objects (int): A private class-level attribute to track the number of objects created.
+                Represents a base entity.
+
+            Attributes:
+                __nb_objects (int): A private class-level attribute to track the number of objects created.
+        """    
         super().__init__(size, size, x, y, id)  # Call the super class with id
         self.width = size
         self.height = size
