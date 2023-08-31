7b99afd536cfff53cbd4b7352e04e4931a1850a8
Your Name <you@example.com>
2023-08-31 19:53:19 +0300

    task 0

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,15 +1,34 @@
 from flask import Flask
 
+# Create a Flask web application instance
 app = Flask(__name__)
+
+# Enable debug mode for the application
 app.config['DEBUG'] = True
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """
+    A route function that responds to the root URL ("/") with a message.
+
+    Returns:
+        str: A greeting message "Hello HBNB!".
+    """
     return 'Hello HBNB!'
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    A route function that responds to the "/hbnb" URL with a message.
+
+    Returns:
+        str: A message "HBNB".
+    """
     return 'HBNB'
 
 if __name__ == "__main__":
+    # Start the Flask development server
+    # Host: 0.0.0.0 (accessible from any IP)
+    # Port: 5000
+    # Debug mode: True (auto-reloads on code changes)
     app.run(host='0.0.0.0', port=5000, debug=True)
